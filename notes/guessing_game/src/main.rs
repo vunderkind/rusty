d334178be5557a5ac15fb0732e9930c7c57be2f5
Justin Irabor <justin.irabor@gmail.com>
2022-04-03 23:03:19 +0100

    complete game

--- notes/guessing_game/src/main.rs
+++ notes/guessing_game/src/main.rs
@@ -17,10 +17,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         
-        let guess: u32 = guess.trim().parse(){
-            Ok(num) => num;
-            Err(_) => continue;
-        }
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
