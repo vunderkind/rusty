f85097e7173182241c22af92cf5567f85671a887
Justin Irabor <justin.irabor@gmail.com>
2022-04-06 23:36:31 +0100

    Data types + integer overflow test

---
+++ notes/data_types/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Scalar types:
+    // Integer
+    // Floating-point numbers
+    // Booleans
+    // Characters
+
+    let mut number:u8 = 256;
+
+    println!("Number is: {}!", number);
+}
