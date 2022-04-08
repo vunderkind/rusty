daff91f6a8cb6247d53b0c8efb468ffc71096f03
Justin Irabor <justin.irabor@gmail.com>
2022-04-08 22:26:56 +0100

    fast commit

--- notes/data_types/src/main.rs
+++ notes/data_types/src/main.rs
@@ -5,11 +5,16 @@ fn main() {
     // Booleans
     // Characters
 
-    let number:u8 = 258;
+    let number:u8 = 250;
 
     println!("Number is: {}!", number);
 
     // Floating point precision
     let x = 2.0; // f64
     let y:f32 = 3.0; // f32
+
+    // tuples
+    let tup = (200, 40, 1.5);
+    let (x, y, z) = tup;
+    println!("The value of x is: {}", x);
 }
---
+++ notes/data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/Users/mac/Documents/work/Sideprojects/rusty/notes/data_types/target/debug/data_types: /Users/mac/Documents/work/Sideprojects/rusty/notes/data_types/src/main.rs
