5fb298db2670c20c82389a297818bc1c55e5359f
Justin Irabor <justin.irabor@gmail.com>
2022-04-09 22:16:22 +0100

    tuples && array

--- notes/data_types/src/main.rs
+++ notes/data_types/src/main.rs
@@ -10,11 +10,16 @@ fn main() {
     println!("Number is: {}!", number);
 
     // Floating point precision
-    let x = 2.0; // f64
-    let y:f32 = 3.0; // f32
+    let _x = 2.0; // f64
+    let _y:f32 = 3.0; // f32
 
     // tuples
     let tup = (200, 40, 1.5);
-    let (x, y, z) = tup;
+    let (x, _, _) = tup;
     println!("The value of x is: {}", x);
+
+    // Array: fixed length, same type
+    let arr:[i32;5] = [1, 2, 3, 4, 5];
+
+    println!("The value of arr[0] is: {}", arr[0]);
 }
