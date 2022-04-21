7ddaa4f84f762acb09c96a89280986f7303d3d23
Justin Irabor <justin.irabor@gmail.com>
2022-04-21 19:53:48 +0100

    fast commit

---
+++ examples/random-number-generator/src/main.rs
@@ -0,0 +1,15 @@
+// Here, we're importing the random range function from the random-number-generator crate.
+use rand::Rng;
+
+fn main() {
+    // it's mut because we're going to change it
+    let mut rng = rand::thread_rng();
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+    println!("Random u8: {}", n1);
+    println!("Random u16: {}", n2);
+    println!("Random u32: {}", rng.gen::<u32>());
+    println!("Random i32: {}", rng.gen::<i32>());
+    println!("Random float: {}", rng.gen::<f64>());
+}
