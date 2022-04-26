36c2ed785ddd25c0f408fde7ffc14dcccd441e75
Justin Irabor <justin.irabor@gmail.com>
2022-04-26 22:07:42 +0100

    fast commit

---
+++ examples/restaurant-functionality-modules/target/debug/deps/restaurant_functionality_modules-c1ed748844bafd6e.d
@@ -0,0 +1,7 @@
+/Users/mac/Documents/work/Sideprojects/rusty/examples/restaurant-functionality-modules/target/debug/deps/restaurant_functionality_modules-c1ed748844bafd6e.rmeta: src/lib.rs
+
+/Users/mac/Documents/work/Sideprojects/rusty/examples/restaurant-functionality-modules/target/debug/deps/librestaurant_functionality_modules-c1ed748844bafd6e.rlib: src/lib.rs
+
+/Users/mac/Documents/work/Sideprojects/rusty/examples/restaurant-functionality-modules/target/debug/deps/restaurant_functionality_modules-c1ed748844bafd6e.d: src/lib.rs
+
+src/lib.rs:
---
+++ examples/restaurant-functionality-modules/target/debug/librestaurant_functionality_modules.d
@@ -0,0 +1,1 @@
+/Users/mac/Documents/work/Sideprojects/rusty/examples/restaurant-functionality-modules/target/debug/librestaurant_functionality_modules.rlib: /Users/mac/Documents/work/Sideprojects/rusty/examples/restaurant-functionality-modules/src/lib.rs
